32f4e53fef0ae56cb58c2da518063ac624a65d60
ryanframes <xmozart.ryan@gmail.com>
2017-10-20 00:21:18 +0700

    Update
    Update halaman input loker

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>LowonganKerja.co.id | Log In</title>  
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">  
+  <link rel="stylesheet" href="css/buttons.css">
+  <link rel="stylesheet" href="css/tab.css">
+</head>
+
+<body>
+  <header style="border-bottom:4px solid #4985e5;">
+    <div class="container">
+      <h1 class="logo"><a href="index.html">lowongankerja.co.id</a></h1>
+    </div>
+  </header>
+  
+  <main>
+
+    <input id="tab1" type="radio" name="tabs" checked>
+    <label for="tab1" style="width:98%">Log In</label>
+    
+    <section id="content1" style="margin:auto; width:50%">
+        <form style="padding-top:20px;" >
+            <input placeholder="Email" name="email" type="text"; style="width:90%;">
+            <br><br>
+            <input placeholder="Kata sandi" name="pswd" type="password"; style="width:90%;">
+            <br><br>
+            <input type="submit" class="btn_simpan" value="Masuk" style="width:90%;">
+        </form>
+    </section>	
+   </main>
+<footer>
+<center>
+	© 2017 LowonganKerja.co.id | Hak cipta dilindungi.
+</center>
+</footer>
+</body>
+</html>
