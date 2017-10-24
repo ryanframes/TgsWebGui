606ff29c82f3ffbb6deb713cf0826e3354c33d0a
ryanframes <xmozart.ryan@gmail.com>
2017-10-24 16:08:28 +0700

    Dashboard Pencari Kerja

--- perusahaan/p_login.php
+++ perusahaan/p_login.php
@@ -3,23 +3,23 @@
 <head>
   <meta charset="UTF-8">
   <title>LowonganKerja.co.id | Log In</title>  
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/style.css">  
-  <link rel="stylesheet" href="css/buttons.css">
-  <link rel="stylesheet" href="css/tab.css">
+  <link rel="stylesheet" href="../css/normalize.css">
+  <link rel="stylesheet" href="../css/style.css">  
+  <link rel="stylesheet" href="../css/buttons.css">
+  <link rel="stylesheet" href="../css/tab.css">
 </head>
 
 <body>
   <header style="border-bottom:4px solid #4985e5;">
     <div class="container">
-      <h1 class="logo"><a href="index.html">lowongankerja.co.id</a></h1>
+      <h1 class="logo"><a href="../index.html">lowongankerja.co.id</a></h1>
     </div>
   </header>
   
   <main>
 
     <input id="tab1" type="radio" name="tabs" checked>
-    <label for="tab1" style="width:98%">Log In Pencari Kerja</label>
+    <label for="tab1" style="width:98%">Log In Perusahaan</label>
     
     <section id="content1" style="margin:auto; width:50%">
         <form style="padding-top:20px;" >
@@ -27,7 +27,7 @@
             <br><br>
             <input placeholder="Kata sandi" name="pswd" type="password"; style="width:90%;">
             <br><br>
-            <input type="submit" class="btn_simpan" value="Masuk" style="width:90%;">
+            <input type="submit" class="btn_simpan" value="Masuk" style="width:90%;" formaction="p_home.html">
         </form>
     </section>	
    </main>
